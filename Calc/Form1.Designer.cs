6b4e1ef66aec5dcc1e99de13b7fc333b20122c44
sunrong <sunrong1990@126.com>
2017-03-15 22:36:08 +0800

    添加FORM 计算器

---
+++ Calc/Form1.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace Calc
+{
+    partial class CalcForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.button_ce = new System.Windows.Forms.Button();
+            this.button_div = new System.Windows.Forms.Button();
+            this.button_mul = new System.Windows.Forms.Button();
+            this.button_sub = new System.Windows.Forms.Button();
+            this.button_num_7 = new System.Windows.Forms.Button();
+            this.button_num_8 = new System.Windows.Forms.Button();
+            this.button_num_9 = new System.Windows.Forms.Button();
+            this.button_num_4 = new System.Windows.Forms.Button();
+            this.button_num_5 = new System.Windows.Forms.Button();
+            this.button_num_6 = new System.Windows.Forms.Button();
+            this.button_num_1 = new System.Windows.Forms.Button();
+            this.button_num_2 = new System.Windows.Forms.Button();
+            this.button_num_3 = new System.Windows.Forms.Button();
+            this.button_add = new System.Windows.Forms.Button();
+            this.button_num_0 = new System.Windows.Forms.Button();
+            this.button_dot = new System.Windows.Forms.Button();
+            this.button_enter = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Enabled = false;
+            this.txtOutput.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtOutput.Location = new System.Drawing.Point(20, 8);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(348, 49);
+            this.txtOutput.TabIndex = 0;
+            // 
+            // button_ce
+            // 
+            this.button_ce.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button_ce.Location = new System.Drawing.Point(20, 67);
+            this.button_ce.Name = "button_ce";
+            this.button_ce.Size = new System.Drawing.Size(78, 42);
+            this.button_ce.TabIndex = 1;
+            this.button_ce.Text = "CE";
+            this.button_ce.UseVisualStyleBackColor = true;
+            this.button_ce.Click += new System.EventHandler(this.button_ce_Click);
+            // 
+            // button_div
+            // 
+            this.button_div.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button_div.Location = new System.Drawing.Point(107, 67);
+            this.button_div.Name = "button_div";
+            this.button_div.Size = new System.Drawing.Size(78, 42);
+            this.button_div.TabIndex = 2;
+            this.button_div.Text = "/";
+            this.button_div.UseVisualStyleBackColor = true;
+            this.button_div.Click += new System.EventHandler(this.button_div_Click);
+            // 
+            // button_mul
+            // 
+            this.button_mul.Font = new System.Drawing.Font("微软雅黑", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button_mul.Location = new System.Drawing.Point(201, 67);
+            this.button_mul.Name = "button_mul";
+            this.button_mul.Size = new System.Drawing.Size(78, 42);
+            this.button_mul.TabIndex = 3;
+            this.button_mul.Text = "*";
+            this.button_mul.UseVisualStyleBackColor = true;
+            this.button_mul.Click += new System.EventHandler(this.button_mul_Click);
+            // 
+            // button_sub
+            // 
+            this.button_sub.Font = new System.Drawing.Font("微软雅黑", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button_sub.Location = new System.Drawing.Point(295, 67);
+            this.button_sub.Name = "button_sub";
+            this.button_sub.Size = new System.Drawing.Size(73, 42);
+            this.button_sub.TabIndex = 4;
+            this.button_sub.Text = "-";
+            this.button_sub.UseVisualStyleBackColor = true;
+            this.button_sub.Click += new System.EventHandler(this.Button_sub_Click);
+            // 
+            // button_num_7
+            // 
+            this.button_num_7.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button_num_7.Location = new System.Drawing.Point(20, 128);
+            this.button_num_7.Name = "button_num_7";
+            this.button_num_7.Size = new System.Drawing.Size(78, 66);
+            this.button_num_7.TabIndex = 5;
+            this.button_num_7.Text = "7";
+            this.button_num_7.UseVisualStyleBackColor = true;
+            this.button_num_7.Click += new System.EventHandler(this.button_num_7_Click);
+            // 
+            // button_num_8
+            // 
+            this.button_num_8.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button_num_8.Location = new System.Drawing.Point(107, 128);
+            this.button_num_8.Name = "button_num_8";
+            this.button_num_8.Size = new System.Drawing.Size(78, 66);
+            this.button_num_8.TabIndex = 6;
+            this.button_num_8.Text = "8";
+            this.button_num_8.UseVisualStyleBackColor = true;
+            this.button_num_8.Click += new System.EventHandler(this.button_num_8_Click);
+            // 
+            // button_num_9
+            // 
+            this.button_num_9.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button_num_9.Location = new System.Drawing.Point(201, 128);
+            this.button_num_9.Name = "button_num_9";
+            this.button_num_9.Size = new System.Drawing.Size(78, 66);
+            this.button_num_9.TabIndex = 7;
+            this.button_num_9.Text = "9";
+            this.button_num_9.UseVisualStyleBackColor = true;
+            this.button_num_9.Click += new System.EventHandler(this.button_num_9_Click);
+            // 
+            // button_num_4
+            // 
+            this.button_num_4.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button_num_4.Location = new System.Drawing.Point(20, 212);
+            this.button_num_4.Name = "button_num_4";
+            this.button_num_4.Size = new System.Drawing.Size(78, 66);
+            this.button_num_4.TabIndex = 8;
+            this.button_num_4.Text = "4";
+            this.button_num_4.UseVisualStyleBackColor = true;
+            this.button_num_4.Click += new System.EventHandler(this.button_num_4_Click);
+            // 
+            // button_num_5
+            // 
+            this.button_num_5.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button_num_5.Location = new System.Drawing.Point(107, 212);
+            this.button_num_5.Name = "button_num_5";
+            this.button_num_5.Size = new System.Drawing.Size(78, 66);
+            this.button_num_5.TabIndex = 9;
+            this.button_num_5.Text = "5";
+            this.button_num_5.UseVisualStyleBackColor = true;
+            this.button_num_5.Click += new System.EventHandler(this.button_num_5_Click);
+            // 
+            // button_num_6
+            // 
+            this.button_num_6.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button_num_6.Location = new System.Drawing.Point(201, 212);
+            this.button_num_6.Name = "button_num_6";
+            this.button_num_6.Size = new System.Drawing.Size(78, 66);
+            this.button_num_6.TabIndex = 10;
+            this.button_num_6.Text = "6";
+            this.button_num_6.UseVisualStyleBackColor = true;
+            this.button_num_6.Click += new System.EventHandler(this.button_num_6_Click);
+            // 
+            // button_num_1
+            // 
+            this.button_num_1.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button_num_1.Location = new System.Drawing.Point(20, 293);
+            this.button_num_1.Name = "button_num_1";
+            this.button_num_1.Size = new System.Drawing.Size(78, 66);
+            this.button_num_1.TabIndex = 12;
+            this.button_num_1.Text = "1";
+            this.button_num_1.UseVisualStyleBackColor = true;
+            this.button_num_1.Click += new System.EventHandler(this.Button_num_1_Click);
+            // 
+            // button_num_2
+            // 
+            this.button_num_2.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button_num_2.Location = new System.Drawing.Point(107, 293);
+            this.button_num_2.Name = "button_num_2";
+            this.button_num_2.Size = new System.Drawing.Size(78, 66);
+            this.button_num_2.TabIndex = 13;
+            this.button_num_2.Text = "2";
+            this.button_num_2.UseVisualStyleBackColor = true;
+            this.button_num_2.Click += new System.EventHandler(this.button_num_2_Click);
+            // 
+            // button_num_3
+            // 
+            this.button_num_3.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button_num_3.Location = new System.Drawing.Point(201, 293);
+            this.button_num_3.Name = "button_num_3";
+            this.button_num_3.Size = new System.Drawing.Size(78, 66);
+            this.button_num_3.TabIndex = 14;
+            this.button_num_3.Text = "3";
+            this.button_num_3.UseVisualStyleBackColor = true;
+            this.button_num_3.Click += new System.EventHandler(this.button_num_3_Click);
+            // 
+            // button_add
+            // 
+            this.button_add.Font = new System.Drawing.Font("微软雅黑", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button_add.Location = new System.Drawing.Point(295, 128);
+            this.button_add.Name = "button_add";
+            this.button_add.Size = new System.Drawing.Size(73, 150);
+            this.button_add.TabIndex = 11;
+            this.button_add.Text = "+";
+            this.button_add.UseVisualStyleBackColor = true;
+            this.button_add.Click += new System.EventHandler(this.Button_add_Click);
+            // 
+            // button_num_0
+            // 
+            this.button_num_0.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button_num_0.Location = new System.Drawing.Point(20, 365);
+            this.button_num_0.Name = "button_num_0";
+            this.button_num_0.Size = new System.Drawing.Size(165, 66);
+            this.button_num_0.TabIndex = 15;
+            this.button_num_0.Text = "0";
+            this.button_num_0.UseVisualStyleBackColor = true;
+            this.button_num_0.Click += new System.EventHandler(this.Button_num_0_Click);
+            // 
+            // button_dot
+            // 
+            this.button_dot.Font = new System.Drawing.Font("微软雅黑", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button_dot.Location = new System.Drawing.Point(201, 365);
+            this.button_dot.Name = "button_dot";
+            this.button_dot.Size = new System.Drawing.Size(78, 66);
+            this.button_dot.TabIndex = 16;
+            this.button_dot.Text = ".";
+            this.button_dot.UseVisualStyleBackColor = true;
+            this.button_dot.Click += new System.EventHandler(this.button_dot_Click);
+            // 
+            // button_enter
+            // 
+            this.button_enter.Font = new System.Drawing.Font("微软雅黑", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button_enter.Location = new System.Drawing.Point(295, 293);
+            this.button_enter.Name = "button_enter";
+            this.button_enter.Size = new System.Drawing.Size(73, 138);
+            this.button_enter.TabIndex = 17;
+            this.button_enter.Text = "=";
+            this.button_enter.UseVisualStyleBackColor = true;
+            this.button_enter.Click += new System.EventHandler(this.Button_enter_Click);
+            // 
+            // CalcForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(392, 445);
+            this.Controls.Add(this.button_enter);
+            this.Controls.Add(this.button_dot);
+            this.Controls.Add(this.button_num_0);
+            this.Controls.Add(this.button_add);
+            this.Controls.Add(this.button_num_3);
+            this.Controls.Add(this.button_num_2);
+            this.Controls.Add(this.button_num_1);
+            this.Controls.Add(this.button_num_6);
+            this.Controls.Add(this.button_num_5);
+            this.Controls.Add(this.button_num_4);
+            this.Controls.Add(this.button_num_9);
+            this.Controls.Add(this.button_num_8);
+            this.Controls.Add(this.button_num_7);
+            this.Controls.Add(this.button_sub);
+            this.Controls.Add(this.button_mul);
+            this.Controls.Add(this.button_div);
+            this.Controls.Add(this.button_ce);
+            this.Controls.Add(this.txtOutput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "CalcForm";
+            this.Text = "计算器";
+            this.Load += new System.EventHandler(this.CalcForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button button_ce;
+        private System.Windows.Forms.Button button_div;
+        private System.Windows.Forms.Button button_mul;
+        private System.Windows.Forms.Button button_sub;
+        private System.Windows.Forms.Button button_num_7;
+        private System.Windows.Forms.Button button_num_8;
+        private System.Windows.Forms.Button button_num_9;
+        private System.Windows.Forms.Button button_num_4;
+        private System.Windows.Forms.Button button_num_5;
+        private System.Windows.Forms.Button button_num_6;
+        private System.Windows.Forms.Button button_num_1;
+        private System.Windows.Forms.Button button_num_2;
+        private System.Windows.Forms.Button button_num_3;
+        private System.Windows.Forms.Button button_add;
+        private System.Windows.Forms.Button button_num_0;
+        private System.Windows.Forms.Button button_dot;
+        private System.Windows.Forms.Button button_enter;
+    }
+}
+
---
+++ Calc/Form1.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Calc
+{
+    public partial class CalcForm : Form
+    {
+        private string strOutput = "";
+        private double lNumFormer = 0;//前一个操作数
+        private double lNumTemp = 0;//临时变量
+        private double lResult = 0;//结果
+        private char cOperator;//操作符
+        private bool bDotClicked = false;//用于标记小数点是否被按下
+        //private uint decimalIndex = 0;//用于标记当前小数点后精度位数，如3.14的decimalIndex==2
+        double lastDecimalNum = 1;//最近一次点击的小数精度，即小数最后一位精度，如0.01
+
+        public CalcForm()
+        {
+            InitializeComponent();
+            //数字按钮0~9
+            button_num_0.Click += new EventHandler(Numbers_Click);
+            button_num_1.Click += new EventHandler(Numbers_Click);
+            button_num_2.Click += new EventHandler(Numbers_Click);
+            button_num_3.Click += new EventHandler(Numbers_Click);
+            button_num_4.Click += new EventHandler(Numbers_Click);
+            button_num_5.Click += new EventHandler(Numbers_Click);
+            button_num_6.Click += new EventHandler(Numbers_Click);
+            button_num_7.Click += new EventHandler(Numbers_Click);
+            button_num_8.Click += new EventHandler(Numbers_Click);
+            button_num_9.Click += new EventHandler(Numbers_Click);
+
+            //运算符+、-、*、/
+            button_add.Click += new EventHandler(Operators_Click);
+            button_sub.Click += new EventHandler(Operators_Click);
+            button_mul.Click += new EventHandler(Operators_Click);
+            button_div.Click += new EventHandler(Operators_Click);
+        }
+
+        private void CalcForm_Load(object sender, EventArgs e)
+        {
+
+        }
+        //****数字按钮0-9
+        private void Button_num_0_Click(object sender, EventArgs e)
+        {
+            //功能实现代码位于Numbers_Click（）函数
+        }
+
+        private void Button_num_1_Click(object sender, EventArgs e)
+        {
+            //功能实现代码位于Numbers_Click（）函数
+        }
+
+        private void button_num_2_Click(object sender, EventArgs e)
+        {
+            //功能实现代码位于Numbers_Click（）函数
+        }
+
+        private void button_num_3_Click(object sender, EventArgs e)
+        {
+            //功能实现代码位于Numbers_Click（）函数
+        }
+
+        private void button_num_4_Click(object sender, EventArgs e)
+        {
+            //功能实现代码位于Numbers_Click（）函数
+        }
+
+        private void button_num_5_Click(object sender, EventArgs e)
+        {
+            //功能实现代码位于Numbers_Click（）函数
+        }
+
+        private void button_num_6_Click(object sender, EventArgs e)
+        {
+            //功能实现代码位于Numbers_Click（）函数
+        }
+
+        private void button_num_7_Click(object sender, EventArgs e)
+        {
+            //功能实现代码位于Numbers_Click（）函数
+        }
+
+        private void button_num_8_Click(object sender, EventArgs e)
+        {
+            //功能实现代码位于Numbers_Click（）函数
+        }
+
+        private void button_num_9_Click(object sender, EventArgs e)
+        {
+            //功能实现代码位于Numbers_Click（）函数
+        }
+
+        //****操作符+、-、*、/
+        private void Button_add_Click(object sender, EventArgs e)
+        {
+            //功能实现代码位于Operators_Click（）函数
+        }
+
+        private void Button_sub_Click(object sender, EventArgs e)
+        {
+            //功能实现代码位于Operators_Click（）函数
+        }
+
+        private void button_mul_Click(object sender, EventArgs e)
+        {
+            //功能实现代码位于Operators_Click（）函数
+        }
+
+        private void button_div_Click(object sender, EventArgs e)
+        {
+            //功能实现代码位于Operators_Click（）函数
+        }
+
+
+        //****等于号=
+        private void Button_enter_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                switch (cOperator)
+                {
+                    case '+':
+                        //计算并检测数据是否越界
+                        checked { lResult = lNumFormer + lNumTemp; }
+                        break;
+                    case '-':
+                        checked { lResult = lNumFormer - lNumTemp; }
+                        break;
+                    case '*':
+                        checked { lResult = lNumFormer * lNumTemp; };
+                        break;
+                    case '/':
+                        checked { lResult = lNumFormer / lNumTemp; };
+                        break;
+                    default:
+                        break;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("运算结果溢出");
+            }
+
+            txtOutput.Text = lResult.ToString();
+            //清空变量
+            lNumFormer = 0;//前一个操作数
+            lNumTemp = 0;//临时变量
+            lResult = 0;//结果
+            cOperator = '\0';//操作符
+            strOutput = "";
+            bDotClicked = false;
+            lastDecimalNum = 1;
+        }
+
+        //****清空键CE
+        private void button_ce_Click(object sender, EventArgs e)
+        {
+            //清空变量
+            lNumFormer = 0;//前一个操作数
+            lNumTemp = 0;//临时变量
+            lResult = 0;//结果
+            cOperator = '\0';//操作符
+            strOutput = "";
+            bDotClicked = false;
+            lastDecimalNum = 1;
+
+            //清空页面显示
+            txtOutput.Text = "";
+        }
+
+        //****小数点.
+        private void button_dot_Click(object sender, EventArgs e)
+        {
+            if (!bDotClicked)
+            {
+                strOutput += ".";
+                txtOutput.Text = strOutput;
+                bDotClicked = true;
+            }
+        }
+
+        //集中处理按钮点击事件
+        //****数字按钮0-9
+        private void Numbers_Click(object sender, EventArgs e)
+        {
+            string strClickNum = ((Button)sender).Text;
+            try
+            {
+                if (bDotClicked)
+                {
+                    //得出点击的小数数值
+                    //decimalIndex++;
+                    lastDecimalNum *= 0.1;
+                    checked { lNumTemp = lNumTemp + long.Parse(strClickNum) * lastDecimalNum; }
+                }
+                else
+                {
+                    checked { lNumTemp = lNumTemp * 10 + long.Parse(strClickNum); }
+                }
+            }
+            catch
+            {
+                //貌似double型并不会轻易溢出
+                MessageBox.Show("数据溢出");
+            }
+            txtOutput.Text = lNumTemp.ToString();
+            strOutput += strClickNum;
+            //strOutput = lNumTemp.ToString();
+            txtOutput.Text = strOutput;
+        }
+
+        //集中处理按钮点击事件
+        //****操作符+、-、*、/
+        private void Operators_Click(object sender, EventArgs e)
+        {
+            cOperator = ((Button)sender).Text[0];//strClickOp长度为1
+            lNumFormer = lNumTemp;
+            lNumTemp = 0;
+            strOutput += cOperator.ToString();
+            txtOutput.Text = strOutput;
+            bDotClicked = false;
+            lastDecimalNum = 1;
+        }
+
+        //键盘按键
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            // if it is a hotkey, return true; otherwise, return false
+            switch (keyData)
+            {
+                case Keys.NumPad0:
+                    button_num_0.Focus();
+                    button_num_0.PerformClick();
+                    return true;
+                case Keys.NumPad1:
+                    button_num_1.Focus();
+                    button_num_1.PerformClick();
+                    return true;
+                case Keys.NumPad2:
+                    button_num_2.Focus();
+                    button_num_2.PerformClick();
+                    return true;
+                case Keys.NumPad3:
+                    button_num_3.Focus();
+                    button_num_3.PerformClick();
+                    return true;
+                case Keys.NumPad4:
+                    button_num_4.Focus();
+                    button_num_4.PerformClick();
+                    return true;
+                case Keys.NumPad5:
+                    button_num_5.Focus();
+                    button_num_5.PerformClick();
+                    return true;
+                case Keys.NumPad6:
+                    button_num_6.Focus();
+                    button_num_6.PerformClick();
+                    return true;
+                case Keys.NumPad7:
+                    button_num_7.Focus();
+                    button_num_7.PerformClick();
+                    return true;
+                case Keys.NumPad8:
+                    button_num_8.Focus();
+                    button_num_8.PerformClick();
+                    return true;
+                case Keys.NumPad9:
+                    button_num_9.Focus();
+                    button_num_9.PerformClick();
+                    return true;
+                case Keys.Add:
+                    button_add.Focus();
+                    button_add.PerformClick();
+                    return true;
+                case Keys.Subtract:
+                    button_sub.Focus();
+                    button_sub.PerformClick();
+                    return true;
+                case Keys.Multiply:
+                    button_mul.Focus();
+                    button_mul.PerformClick();
+                    return true;
+                case Keys.Divide:
+                    button_div.Focus();
+                    button_div.PerformClick();
+                    return true;
+                case Keys.Enter:
+                    button_enter.Focus();
+                    button_enter.PerformClick();
+                    return true;
+                case Keys.Escape:
+                    button_ce.Focus();
+                    button_ce.PerformClick();
+                    return true;
+                case Keys.Decimal:
+                    button_dot.Focus();
+                    button_dot.PerformClick();
+                    return true;
+                default:
+                    break;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+    }
+}
