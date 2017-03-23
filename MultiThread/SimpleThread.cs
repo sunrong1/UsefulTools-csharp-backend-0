c1e39d46cbe92be702871b1d6cef45f978b8f269
sunrong <sunrong1990@126.com>
2017-03-24 00:11:17 +0800

    Thread操作练习

---
+++ MultiThread/SimpleThread.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultiThread
+{
+    public class SimpleThread
+    {
+        public void ShowMessage()
+        {
+            string message = string.Format("Async threadId is :{0}",
+                                            Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine(message);
+
+            for (int n = 0; n < 10; n++)
+            {
+                Thread.Sleep(300);
+                Console.WriteLine("The number is:" + n.ToString());
+            }
+        }
+        public bool Show()
+        {
+            Thread show = new Thread(ShowMessage);
+            show.Start();
+            Thread.Sleep(2000);
+            return true;
+        }
+    }
+}
