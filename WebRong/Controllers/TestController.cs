ed23a918020de1432efa0fc34d13e27b821632ba
sunrong <sunrong1990@126.com>
2017-05-01 17:16:35 +0800

    Add ViewModels layer

--- WebRong/Controllers/TestController.cs
+++ WebRong/Controllers/TestController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebRong.BusinessLayer;
 using WebRong.Models;
+using WebRong.ViewModels;
 
 namespace WebRong.Controllers
 {
@@ -21,15 +23,58 @@ namespace WebRong.Controllers
         }
         public ActionResult GetView()
         {
-            Employee emp = new Employee();
-            emp.FirstName = "Sukesh";
-            emp.LastName = "Marla";
-            emp.Salary = 20000;
-            ViewBag.Employee = emp;
-            //ViewData["Employee"] = emp;
-            return View("MyView");
-            //return Content("Hi Welcome");
-            //return View("MyView");
+            //Employee emp = new Employee();
+            //emp.FirstName = "Sukesh";
+            //emp.LastName = "Marla";
+            //emp.Salary = 20000;
+
+            //EmployeeViewModel vmEmp = new EmployeeViewModel();
+            //vmEmp.EmployeeName = emp.FirstName + " " + emp.LastName;
+            //vmEmp.Salary = emp.Salary.ToString("C");
+            //if (emp.Salary > 15000)
+            //{
+            //    vmEmp.SalaryColor = "yellow";
+            //}
+            //else
+            //{
+            //    vmEmp.SalaryColor = "green";
+            //}
+
+            //vmEmp.UserName = "Admin";
+            //return View("MyView", vmEmp);
+
+            EmployeeListViewModel employeeListViewModel =
+            new EmployeeListViewModel();
+
+            EmployeeBusinessLayer empBal =
+                     new EmployeeBusinessLayer();
+            List<Employee> employees = empBal.GetEmployees();
+
+            List<EmployeeViewModel> empViewModels =
+                     new List<EmployeeViewModel>();
+
+            foreach (Employee emp in employees)
+            {
+                EmployeeViewModel empViewModel =
+                      new EmployeeViewModel();
+
+                empViewModel.EmployeeName =
+                      emp.FirstName + " " + emp.LastName;
+
+                empViewModel.Salary = emp.Salary.ToString("C");
+                if (emp.Salary > 15000)
+                {
+                    empViewModel.SalaryColor = "yellow";
+                }
+                else
+                {
+                    empViewModel.SalaryColor = "green";
+                }
+                empViewModels.Add(empViewModel);
+            }
+            employeeListViewModel.Employees = empViewModels;
+            employeeListViewModel.UserName = "Admin";
+            return View("MyView", employeeListViewModel);
         }
     }
 }
---
+++ WebRong/ViewModels/EmployeeListViewModel.cs
@@ -0,0 +1,14 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebRong.ViewModels;
+
+namespace WebRong.ViewModels
+{
+    public class EmployeeListViewModel
+    {
+        public List<EmployeeViewModel> Employees { get; set; }
+        public string UserName { get; set; }
+    }
+}
